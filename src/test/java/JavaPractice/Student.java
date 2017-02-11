b1c0f3d2add857b06734496d3cc31fcc63142a33
hasini <hasini@DESKTOP-EOG4SAV.fios-router.home>
2017-02-11 16:13:38 -0500

    added

---
+++ src/test/java/JavaPractice/Student.java
@@ -0,0 +1,18 @@
+package JavaPractice;
+
+public class Student {
+
+	int stuno;
+	String name;
+	int age;
+	
+	public Student(int stno,String name,int age)
+	{
+		this.stuno=stno;
+		this.name=name;
+		this.age=age;
+	}
+
+
+	
+}
